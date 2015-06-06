0c73eefdab5570d8d7f0a65e85354a123149abd0
Antonio <Antonio@Antonio-PC>
2015-06-06 17:36:03 +0200

    Proyecto Comparador

--- frontEnd/phpEnviarCorreo.php
+++ frontEnd/phpEnviarCorreo.php
@@ -7,16 +7,13 @@ $email = Leer::post("email");
 $asunto = Leer::post("asunto");
 $mensaje = Leer::post("mensaje");
 
-//cabeceras
 $headers = "MIME-Version: 1.0\r\n";
 $headers .= "Content-type: text/html; charset=iso-8859-1\r\n";
-//dirección del remitente 
+
 $headers .= "From: " . $nombre . " <" . $email . ">\r\n";
 
-//Envío de email
 $r = mail("proyectocomparador@gmail.com", $asunto, $mensaje, $headers);
 
-//echo $r;
 if($r)
 {
     header("Location: ../contacta.php?r=Mensaje Enviado");
